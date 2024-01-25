48a5d150800eb6f34adc6af2a16f6c906c49862f
Hashir <hashir0601.be21@chitkara.edu.in>
2024-01-25 18:26:32 +0530

    refixed layout.tsx

--- src/components/site/Layout.tsx
+++ src/components/site/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="relative  h-[130vh] min-h-[100vh] bg-white font-ubuntu dark:bg-zinc-900">
+		<div className="relative   min-h-[100vh] bg-white font-ubuntu dark:bg-zinc-900">
 			<Navbar />
 			<main className="p-10 sm:px-16 lg:px-44">
 				<div className="mx-auto max-w-3xl space-y-20">{children}</div>
